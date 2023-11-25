2273b5976f25a855b283905070dca6fe9386a136
star-candy <el0710@naver.com>
2023-11-26 04:08:17 +0900

    refactor(baseball): <전체 테스트 성공>

--- src/main/java/exception/GameException.java
+++ src/main/java/exception/GameException.java
@@ -1,5 +1,7 @@
 package exception;
 
+import constants.Description;
+
 public class GameException {
 
     private final int inputValue;
@@ -16,12 +18,37 @@ public class GameException {
     }
 
     private void longInput() {
+
+        String numberString = String.valueOf(inputValue);
+        if (numberString.length() > 3) {
+            throw new IllegalArgumentException();
+        }
     }
 
     private void sameInput() {
+        String numberString = String.valueOf(inputValue);
+        char firstDigit = numberString.charAt(0);
+        char secondDigit = numberString.charAt(1);
+        char thirdDigit = numberString.charAt(2);
+        if (firstDigit == secondDigit || secondDigit == thirdDigit || firstDigit == thirdDigit) {
+            System.out.println(Description.SAME_INPUT.getMessage());
+            throw new IllegalArgumentException();
+        }
     }
 
+
     private void improperInput() {
+        try {
+            String numberString = String.valueOf(inputValue);
+            char firstDigit = numberString.charAt(0);
+            char secondDigit = numberString.charAt(1);
+            char thirdDigit = numberString.charAt(2);
+            if (firstDigit == '0' || secondDigit == '0' || thirdDigit == '0') {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(Description.LONG_INPUT.getMessage());
+        }
     }
 
 }
--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,20 +2,24 @@ package view;
 
 import camp.nextstep.edu.missionutils.Console;
 import constants.Description;
+import exception.GameException;
+import exception.RestartException;
 
 public class InputView {
 
     public int ballNumber() {
         int inputNumber;
+        String inputString;
         try {
             System.out.printf(Description.INPUT_NUMBER.getMessage());
-            inputNumber = Integer.parseInt(Console.readLine());
-            //new GameException(inputNumber);
+            inputString = Console.readLine();
+            inputNumber = Integer.parseInt(inputString);
+            new GameException(inputNumber);
             System.out.println(inputNumber);
             return inputNumber;
         } catch (IllegalArgumentException e) {
-            System.out.println(Description.NOT_NUMBER.getMessage());
-            return ballNumber();
+            throw new IllegalArgumentException();
+
         }
     }
 
@@ -24,7 +28,7 @@ public class InputView {
         try {
             System.out.printf(Description.RESTART_COUNT.getMessage());
             inputNumber = Integer.parseInt(Console.readLine());
-            //new RestartException(inputNumber);
+            new RestartException(inputNumber);
             System.out.println(inputNumber);
             return inputNumber;
         } catch (IllegalArgumentException e) {
